b42b7933b36ce81633d35a9bb95c010dc13fd0f4
Kibreab Chanyalew <affablemelon@gmail.com>
2025-11-24 00:08:19 +0300

    remove refactored files

--- scripts/time_analys.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def articles_per_day(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Computes number of articles published per day.
-    """
-    df["date_only"] = df["date"].dt.date
-    return df.groupby("date_only").size().reset_index(name="count")
